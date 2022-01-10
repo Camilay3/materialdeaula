acd72cc2bfe8a35876dd790402d2fb8fd1d88679
Camila Azevedo <92113919+Camilay3@users.noreply.github.com>
2022-01-10 20:47:19 -0300

    Terceira aula

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var b1 = document.getElementById("baula01");
 var b2 = document.getElementById("baula02");
-/* var b3 = document.getElementById("baula03");
+var b3 = document.getElementById("baula03");
 var b4 = document.getElementById("baula04");
 var b5 = document.getElementById("baula05");
-var b6 = document.getElementById("baula06"); */
+var b6 = document.getElementById("baula06");
 
 function textoEntrar(bx, txt){
     bx.innerText= txt
@@ -26,15 +26,15 @@ function sair01(){
 }
 
 function entrar02(){
-  textoEntrar(b2, 'Cores e \nestruturas');
+    textoEntrar(b2, 'Cores e \nestruturas');
 }
 
 function sair02(){
-  textoSair(b2, 'Aula 02');
+    textoSair(b2, 'Aula 02');
 }
 
-/*function entrar03(){
-    textoEntrar(b3, '');
+function entrar03(){
+    textoEntrar(b3, 'Bibliotecas \ne funções');
 }
 
 function sair03(){
@@ -42,7 +42,7 @@ function sair03(){
 }
 
 function entrar04(){
-    textoEntrar(b4, '');
+    textoEntrar(b4, 'Listas \ne tuplas');
 }
 
 function sair04(){
@@ -50,7 +50,7 @@ function sair04(){
 }
 
 function entrar05(){
-    textoEntrar(b5, '');
+    textoEntrar(b5, 'Dicionários');
 }
 
 function sair05(){
@@ -58,9 +58,9 @@ function sair05(){
 }
 
 function entrar06(){
-    textoEntrar(b6, '');
+    textoEntrar(b6, 'Interface \ngráfica');
 }
 
 function sair06(){
     textoSair(b6, 'Aula 06');
-}*/
+}
